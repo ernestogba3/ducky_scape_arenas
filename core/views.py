e7dfe4827213a682d4a16a7be9009088226c6f77
Mars <pinilla.mars@gmail.com>
2026-09-18 12:52:37 +0200

    views update

--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
+from accounts import models
 
+player = models.ForeignKey(
+settings.AUTH_USER_MODEL,
+on_delete=models.CASCADE,
+related_name="escape_sessions",
+)
 
 def home(request):
     return render(request, 'core/home.html')
